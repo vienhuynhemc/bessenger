e471e75b46266a4ebb9ae0b70b19c44e2b6c6b32
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-05-23 18:20:34 +0700

    Tạo 6 trang chính của app chat

--- app-chat/src/app/app-routing.module.ts
+++ app-chat/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+// Các trang của chat app
+import { LoginPageComponent } from './view/login-page/login-page.component';
+import { NotFoundPageComponent } from './view/not-found-page/not-found-page.component';
+import { HomePageComponent } from './view/home-page/home-page.component';
+import { FriendsPageComponent } from './view/friends-page/friends-page.component';
+import { SettingPageComponent } from './view/setting-page/setting-page.component';
+import { ChatPageComponent } from './view/chat-page/chat-page.component';
+
+// Của Angular
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  { path: "chat-page", component: ChatPageComponent },
+  { path: "setting-page", component: SettingPageComponent },
+  { path: "friends-page", component: FriendsPageComponent },
+  { path: "home-page", component: HomePageComponent },
+  { path: "login-page", component: LoginPageComponent },
+  { path: "**", component: NotFoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app-chat/src/app/app.module.ts
+++ app-chat/src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Trang index
 import { AppComponent } from './app.component';
+
+// Các trang của chat app
+import { ChatPageComponent } from './view/chat-page/chat-page.component';
+import { FriendsPageComponent } from './view/friends-page/friends-page.component';
+import { HomePageComponent } from './view/home-page/home-page.component';
+import { SettingPageComponent } from './view/setting-page/setting-page.component';
+import { NotFoundPageComponent } from './view/not-found-page/not-found-page.component';
+import { LoginPageComponent } from './view/login-page/login-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    // Trang index
+    AppComponent,
+
+    // Các trang của chat app
+    ChatPageComponent,
+    HomePageComponent,
+    SettingPageComponent,
+    FriendsPageComponent,
+    NotFoundPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
