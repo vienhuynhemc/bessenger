96f74a41506696ffb85cea0e3737491bb2705825
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-05-24 13:54:52 +0700

    Tổ chức lại cây thư mục

--- app-chat/src/app/app-routing.module.ts
+++ app-chat/src/app/app-routing.module.ts
@@ -1,22 +1,13 @@
-// Các trang của chat app
-import { LoginPageComponent } from './view/login-page/login-page.component';
+// 2 trang main - 404
+import { MainPageComponent } from './view/main-page/main-page.component';
 import { NotFoundPageComponent } from './view/not-found-page/not-found-page.component';
-import { HomePageComponent } from './view/home-page/home-page.component';
-import { FriendsPageComponent } from './view/friends-page/friends-page.component';
-import { SettingPageComponent } from './view/setting-page/setting-page.component';
-import { ChatPageComponent } from './view/chat-page/chat-page.component';
-
 // Của Angular
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", component: HomePageComponent },
-  { path: "chat-page", component: ChatPageComponent },
-  { path: "setting-page", component: SettingPageComponent },
-  { path: "friends-page", component: FriendsPageComponent },
-  { path: "home-page", component: HomePageComponent },
-  { path: "login-page", component: LoginPageComponent },
+  { path: "", component: MainPageComponent },
+  { path: "bessenger", component: MainPageComponent },
   { path: "**", component: NotFoundPageComponent },
 ];
 
