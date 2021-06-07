9116f36a9b4d398de4624211e0bc4582e4d36bc4
unknown <18130126@st.hcmuaf.edu.vn>
2021-06-07 20:51:46 +0700

    File send

--- app-chat/src/app/view/main-page/chat-page/file-send/file-send.component.ts
+++ app-chat/src/app/view/main-page/chat-page/file-send/file-send.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-file-send',
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-send.component.scss']
 })
 export class FileSendComponent implements OnInit {
+  name: string = 'William Halaand';
+  career: string = 'CEO & Founded';
+  avatar: string = 'assets/images/man.png';
+  amountMedia: number = 1;
+  amountFiles: number = 5;
+  //type load
+  typeZip: string = ".zip";
+  typeDocx: string = ".docx";
+  typeTxt: string = ".txt";
+  typePdf: string = ".pdf";
 
+  @Input() friends_list!: any[];
+
+  @Input() selectedUser!: number;
   constructor() { }
 
   ngOnInit(): void {
