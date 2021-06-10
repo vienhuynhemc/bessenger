c48b60ceeeb58dff3e83a212cd5ca943f7c6c761
unknown <18130054@st.hcmuaf.edu.vn>
2021-06-10 18:40:18 +0700

    hoàn thành cài đặt componen contacts, request-add-friends-list

---
+++ app-chat/src/app/view/main-page/friends-page/groups-chat-list/groups-chat-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-groups-chat-list',
+  templateUrl: './groups-chat-list.component.html',
+  styleUrls: ['./groups-chat-list.component.scss']
+})
+export class GroupsChatListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
