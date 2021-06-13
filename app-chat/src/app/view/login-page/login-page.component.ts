68fca5dfb4ff5d7be08d38a8b19ea820dbe861d5
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-06-14 01:29:33 +0700

    Hoàn thành kiểm tra thiếu thông tin khi đăng ký, đã có thông báo hiện ngay lúc thiếu thông tin, hoàn thành giao diện đăng ký ở trang đăng nhập

--- app-chat/src/app/view/login-page/login-page.component.ts
+++ app-chat/src/app/view/login-page/login-page.component.ts
@@ -11,6 +11,7 @@ export class LoginPageComponent implements OnInit {
 
   public userName: string;
   public passWord: string;
+  public ten: string;
   public countSlide: number;
 
   constructor(private router: Router, private login_service: LoginService) {
@@ -34,6 +35,43 @@ export class LoginPageComponent implements OnInit {
     this.router.navigate(["/bessenger"]);
   }
 
+  dangKyTaoKhoanChuyenComponent() {
+    document.getElementById("dang-nhap").style.display = "none";
+    document.getElementById("dang-ky-2").style.display = "none";
+    document.getElementById("dang-ky").style.display = "block";
+    document.getElementById("dang-nhap-2").style.display = "flex";
+    this.reset();
+  }
+
+  dangNhapChuyenComponent() {
+    document.getElementById("dang-nhap").style.display = "block";
+    document.getElementById("dang-ky-2").style.display = "flex";
+    document.getElementById("dang-ky").style.display = "none";
+    document.getElementById("dang-nhap-2").style.display = "none";
+    this.reset();
+  }
+
+  reset() {
+    this.userName = "";
+    this.passWord = "";
+    this.ten = "";
+    let elements = document.getElementById("level-password").children;
+    elements[3].textContent = "Hãy nhập mật khẩu";
+    (<HTMLElement>elements[3]).style.color = "#e2e2e2";
+    (<HTMLElement>elements[0]).style.background = "#e2e2e2";
+    (<HTMLElement>elements[1]).style.background = "#e2e2e2";
+    (<HTMLElement>elements[2]).style.background = "#e2e2e2";
+
+    document.getElementById("dk-email").style.border = "1px solid #e2e2e2";
+    document.getElementById("dk-error-1").style.display = "none";
+
+    document.getElementById("dk-ten").style.border = "1px solid #e2e2e2";
+    document.getElementById("dk-error-2").style.display = "none";
+
+    document.getElementById("dk-password").style.border = "1px solid #e2e2e2";
+    document.getElementById("dk-error-3").style.display = "none";
+  }
+
 
   hiddenAndShowPassword(e: HTMLInputElement, eye: HTMLElement): void {
     if (e.type == 'text') {
@@ -45,6 +83,49 @@ export class LoginPageComponent implements OnInit {
       eye.classList.add('fa-eye-slash');
       eye.classList.remove('fa-eye');
     }
+  }
+
+  dkTen(value) {
+    if (value.trim().length > 0) {
+      document.getElementById("dk-ten").style.border = "1px solid #e2e2e2";
+      document.getElementById("dk-error-2").style.display = "none";
+    }else{
+      document.getElementById("dk-ten").style.border = "1px solid #ff7b5c";
+      document.getElementById("dk-error-2").style.display = "block";
+    }
+  }
+
+  dkEmail(value) {
+    if (value.trim().length > 0) {
+      document.getElementById("dk-email").style.border = "1px solid #e2e2e2";
+      document.getElementById("dk-error-1").style.display = "none";
+    }else{
+      document.getElementById("dk-email").style.border = "1px solid #ff7b5c";
+      document.getElementById("dk-error-1").style.display = "block";
+    }
+  }
+
+  dangKy(): void {
+    let email = this.userName.trim();
+    let ten = this.ten.trim();
+    let mat_khau = this.passWord;
+    let count = 0;
+    if (email.length == 0) {
+      count++;
+      document.getElementById("dk-email").style.border = "1px solid #ff7b5c";
+      document.getElementById("dk-error-1").style.display = "block";
+    }
+    if (ten.length == 0) {
+      count++;
+      document.getElementById("dk-ten").style.border = "1px solid #ff7b5c";
+      document.getElementById("dk-error-2").style.display = "block";
+    }
+    if (mat_khau.length == 0) {
+      count++;
+      document.getElementById("dk-password").style.border = "1px solid #ff7b5c";
+      document.getElementById("dk-error-3").style.display = "block";
+    }
+    console.log(count);
   }
 
   setDelay(times: any) {
@@ -63,6 +144,62 @@ export class LoginPageComponent implements OnInit {
       // repeate
       this.setDelay(times);
     }, times);
+  }
+
+  changePassword(value: any) {
+    let i = 0;
+    let isHaveNumber = false;
+    let isHaveUpcase = false;
+    while (i <= value.length) {
+      let character = value.charAt(i);
+      if (character == character.toUpperCase() && isNaN(character)) {
+        isHaveUpcase = true;
+      }
+      i++;
+    }
+    isHaveNumber = this.hasNumber(value);
+    let elements = document.getElementById("level-password").children;
+    if (!isHaveNumber && !isHaveUpcase && value.length > 0) {
+      elements[3].textContent = "Yếu";
+      (<HTMLElement>elements[3]).style.color = "#ff7b5c";
+      (<HTMLElement>elements[0]).style.background = "#ff7b5c";
+      (<HTMLElement>elements[1]).style.background = "#e2e2e2";
+      (<HTMLElement>elements[2]).style.background = "#e2e2e2";
+
+      document.getElementById("dk-password").style.border = "1px solid #e2e2e2";
+      document.getElementById("dk-error-3").style.display = "none";
+    } else if ((!isHaveNumber && isHaveUpcase) || (!isHaveUpcase && isHaveNumber)) {
+      elements[3].textContent = "Trung bình";
+      (<HTMLElement>elements[3]).style.color = "yellow";
+      (<HTMLElement>elements[0]).style.background = "yellow";
+      (<HTMLElement>elements[1]).style.background = "yellow";
+      (<HTMLElement>elements[2]).style.background = "#e2e2e2";
+
+      document.getElementById("dk-password").style.border = "1px solid #e2e2e2";
+      document.getElementById("dk-error-3").style.display = "none";
+    } else if (isHaveNumber && isHaveUpcase) {
+      elements[3].textContent = "Mạnh";
+      (<HTMLElement>elements[3]).style.color = "#20e820";
+      (<HTMLElement>elements[0]).style.background = "#20e820";
+      (<HTMLElement>elements[1]).style.background = "#20e820";
+      (<HTMLElement>elements[2]).style.background = "#20e820";
+
+      document.getElementById("dk-password").style.border = "1px solid #e2e2e2";
+      document.getElementById("dk-error-3").style.display = "none";
+    } else {
+      elements[3].textContent = "Hãy nhập mật khẩu";
+      (<HTMLElement>elements[3]).style.color = "#e2e2e2";
+      (<HTMLElement>elements[0]).style.background = "#e2e2e2";
+      (<HTMLElement>elements[1]).style.background = "#e2e2e2";
+      (<HTMLElement>elements[2]).style.background = "#e2e2e2";
+    
+      document.getElementById("dk-password").style.border = "1px solid #ff7b5c";
+      document.getElementById("dk-error-3").style.display = "block";
+    }
+  }
+
+  hasNumber(myString) {
+    return /\d/.test(myString);
   }
 
 }
