f0e00bdd128e7efdb5b7e3f2963e5403fed318c7
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-06-22 07:44:34 +0700

    Hoàn thành hiển thị nội dung tin nhắn cuối cùng, fix ui khi không có nội dung

--- app-chat/src/app/models/chat-page/chat-page-friends-page/chat_page_friends_object_left.ts
+++ app-chat/src/app/models/chat-page/chat-page-friends-page/chat_page_friends_object_left.ts
@@ -103,4 +103,68 @@ export class ChatPageFriendsObjectLeft {
         return this.cuoc_tro_truyen.loai_cuoc_tro_truyen == "nhom";
     }
 
+    public getNoiDungCuoiCung(): string {
+        let result = "";
+        if (this.cuoc_tro_truyen.tin_nhan != null && this.cuoc_tro_truyen.tin_nhan.length > 0) {
+            let index = 0;
+            let ngay_gui_max = this.cuoc_tro_truyen.tin_nhan[0].ngay_gui;
+            for (let i = 0; i < this.cuoc_tro_truyen.tin_nhan.length; i++) {
+                if (this.cuoc_tro_truyen.tin_nhan[i].ngay_gui > ngay_gui_max) {
+                    ngay_gui_max = this.cuoc_tro_truyen.tin_nhan[i].ngay_gui;
+                    index = i;
+                }
+            }
+            let ten = this.getTenNguoiGui(index);
+            switch (this.cuoc_tro_truyen.tin_nhan[index].loai_tin_nhan) {
+                case "gui_text":
+                    if (ten == "Bạn") {
+                        result = this.cuoc_tro_truyen.tin_nhan[index].noi_dung;
+                    } else {
+                        result = ten + ": " + this.cuoc_tro_truyen.tin_nhan[index].noi_dung;
+                    }
+                    break;
+                case "thong_bao":
+                    result = ten + " " + this.cuoc_tro_truyen.tin_nhan[index].noi_dung;
+                    break;
+                case "gui_hinh":
+                    result = ten + " gửi một hình ảnh";
+                    break;
+                case "gui_video":
+                    result = ten + " gửi một video";
+                    break;
+                case "gui_ghi_am":
+                    result = ten + " gửi một tin nhắn thoại";
+                    break;
+                case "gui_file":
+                    result = ten + " gửi một tệp";
+                    break;
+                case "thu_hoi":
+                    result = ten + " thu hồi một tin nhắn";
+                    break;
+                case "phan_hoi":
+                    result = ten + " phản hồi một tin nhắn";
+                    break;
+            }
+        }
+        if (result.length > 25) {
+            result = result.substring(0, 22) + "...";
+        }
+        return result;
+    }
+
+    public getTenNguoiGui(index: number): string {
+        let ma_tai_khoan = JSON.parse(localStorage.getItem("ma_tai_khoan_dn"));
+        if (this.cuoc_tro_truyen.tin_nhan[index].ma_tai_khoan == ma_tai_khoan) {
+            return "Bạn";
+        } else {
+            if (this.cuoc_tro_truyen.tin_nhan[index].ten != null) {
+                let ten: string = this.cuoc_tro_truyen.tin_nhan[index].ten.trim();
+                let array: string[] = ten.split(" ");
+                return array[array.length - 1];
+            } else {
+                return "";
+            }
+        }
+    }
+
 }
