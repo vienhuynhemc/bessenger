3719d74a1a67501d57f65dda727e7f0b5bfbf414
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-06-28 04:33:31 +0700

    Hoàn thành cơ bản giao diện chat chính giữa, đã xử lý cao nhất 5 hàng các component tự chia sẻ height, xử lý scroll cho windown

--- app-chat/src/app/service/chat-page/chat-page-chat-page/chat-page-chat-page-footer/messenger-footer.service.ts
+++ app-chat/src/app/service/chat-page/chat-page-chat-page/chat-page-chat-page-footer/messenger-footer.service.ts
@@ -6,8 +6,16 @@ import { AngularFireDatabase } from '@angular/fire/database';
 })
 export class MessengerFooterService {
 
+  public chenh_lech_height: number;
+
   constructor(
     private db: AngularFireDatabase,
-  ) { }
+  ) {
+    this.chenh_lech_height = 0;
+  }
+
+  public getHeight(): string {
+    return (633 - this.chenh_lech_height) + "px";
+  }
 
 }
--- app-chat/src/app/view/main-page/chat-page/messenger/messenger-footer/messenger-footer.component.ts
+++ app-chat/src/app/view/main-page/chat-page/messenger/messenger-footer/messenger-footer.component.ts
@@ -10,12 +10,15 @@ export class MessengerFooterComponent implements OnInit {
 
   // Nội dung tin nhắn
   public tin_nhan: string;
+  // Kích thước trc thay đổi
+  public last_height: number;
 
   constructor(
     public messenger_footer_service: MessengerFooterService
   ) { }
 
   ngOnInit(): void {
+    this.last_height = 22;
   }
 
   public inputMessenger(value: string) {
@@ -24,9 +27,22 @@ export class MessengerFooterComponent implements OnInit {
     let parent_input = document.getElementById("parent_input");
     if (parent_input.offsetHeight) {
       if (parent_input.offsetHeight > 22) {
-        parent_input.style.marginBottom = "10px";
+        if (parent_input.offsetHeight != this.last_height) {
+          parent_input.style.marginBottom = "10px";
+          this.messenger_footer_service.chenh_lech_height = parent_input.offsetHeight - 17;
+          if (this.last_height == 22) {
+            window.scrollTo(0, window.pageYOffset + (parent_input.offsetHeight - this.last_height) + 5);
+          } else {
+            window.scrollTo(0, window.pageYOffset + (parent_input.offsetHeight - this.last_height));
+          }
+          this.last_height = parent_input.offsetHeight
+        }
       } else {
-        parent_input.style.marginBottom = "5px";
+        if (this.last_height != 22) {
+          this.last_height = 22;
+          parent_input.style.marginBottom = "5px";
+          this.messenger_footer_service.chenh_lech_height = 0;
+        }
       }
     }
   }
