703800d2115ffe949e371329a350f1e09279e9a1
unknown <18130054@st.hcmuaf.edu.vn>
2021-07-29 14:30:59 +0700

    Hoàn thành trang chủ

--- app-chat/src/app/view/main-page/home-page/home-page.component.ts
+++ app-chat/src/app/view/main-page/home-page/home-page.component.ts
@@ -1,22 +1,91 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MainPageService } from './../../../service/main-page/main-page.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-
-  constructor(
-    private main_page_service: MainPageService,
-  ) { }
-
+    
+    constructor(private main_page_service: MainPageService, private router: Router) {}
+  selectedSlide: number = 1;
+  timeOut: any = 0;
   ngOnInit(): void {
     setTimeout(() => {
       this.main_page_service.reset();
       this.main_page_service.selectTrangChu();
     }, 0);
+    this.reset();
+    this.setChangeSlide();
   }
+  reset() {
+    let slide1 = document.getElementById('slide-1');
+    let slide2 = document.getElementById('slide-2');
+    let slide3 = document.getElementById('slide-3');
+    slide3.classList.remove('slide-selected')
+    slide2.classList.remove('slide-selected')
+    slide1.classList.add('slide-selected');
+  }
+  setChangeSlide() {
+    let slide1 = document.getElementById('slide-1');
+    let slide2 = document.getElementById('slide-2');
+    let slide3 = document.getElementById('slide-3');
+    
+    this.timeOut = setTimeout(() => {
+      if (this.selectedSlide == 0) {
+        slide3.classList.remove('slide-selected')
+        slide1.classList.add('slide-selected');
+        this.selectedSlide = 1;
+        
+      } else if (this.selectedSlide == 1) {
+        slide1.classList.remove('slide-selected')
+        slide2.classList.add('slide-selected');
+        let video = <HTMLVideoElement>document.getElementById('slide-2-video')
+        video.load()
+        this.selectedSlide = 2;
 
+      } else {
+        slide2.classList.remove('slide-selected')
+        slide3.classList.add('slide-selected');
+        this.selectedSlide = 0;
+
+      }
+      this.setChangeSlide()
+    }, 7000);
+  }
+  slideClick(slide:number) {
+    let slide1 = document.getElementById('slide-1');
+    let slide2 = document.getElementById('slide-2');
+    let slide3 = document.getElementById('slide-3');
+    clearTimeout(this.timeOut)
+    if(slide == 0) {
+      slide3.classList.remove('slide-selected')
+      slide2.classList.remove('slide-selected')
+      slide1.classList.add('slide-selected');
+      this.selectedSlide = 1;
+    } else if(slide == 1) {
+      slide1.classList.remove('slide-selected')
+      slide3.classList.remove('slide-selected')
+      slide2.classList.add('slide-selected');
+      let video = <HTMLVideoElement>document.getElementById('slide-2-video')
+      video.load()
+      this.selectedSlide = 2;
+    } else {
+      slide2.classList.remove('slide-selected')
+      slide1.classList.remove('slide-selected')
+      slide3.classList.add('slide-selected');
+      this.selectedSlide = 0;
+    }
+    this.setChangeSlide()
+  }
+  moveToMessage() {
+      this.router.navigate(['/bessenger/tin-nhan/danh-sach']);
+  }
+  
+  moveToRecomend() {
+    this.router.navigate(['/bessenger/ban-be/de-xuat']);
+
+  }
 }
